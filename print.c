180e71e10e5278166add82523ae5c493fc905713
9801705802 <karkiprabin496@gmail.com>
2016-11-14 11:14:06 +0545

    Add files via upload
     print your name

---
+++ print.c
@@ -0,0 +1,18 @@
+/*
+Author/student :prabin karki
+Subject:program Fundamental
+Roll no :11
+Lab sheet No :1
+Program:WAP to print your name
+Date: November 14 , 2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ printf("My name is prabin");
+ getch();
+}
+
+
